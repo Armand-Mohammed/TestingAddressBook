31a1eb7215949be452f18a1ee6acfcc57ba3e5fe
Armand-Mohammed <31212173+Armand-Mohammed@users.noreply.github.com>
2020-03-11 10:53:27 -0400

    STP-1 Tidy Up Test Code and test PRs for concurrency and peer review (#1)

--- test/AddressBook/AddressBookTest.java
+++ test/AddressBook/AddressBookTest.java
@@ -7,18 +7,28 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 class AddressBookTest {
 
     AddressBook addressBook = new AddressBook();
-    Person instance = new Person("Armand", "Mohammed", "Street address", "Fort Myers", "FL", "33317", "9545130066");
+    Person instance = new Person("Armand", "Mohammed", "1660 sw 48th ave", "Fort Myers", "FL", "33317", "9545130066");
 
     @Test
     void add() {
+        // Act
         addressBook.add(instance);
+
+        // Assert
         assertEquals(instance, addressBook.get(0));
     }
 
     @Test
     void remove() {
+        // Arrange
+        Person expectedPerson = new Person("Armand", "Mohammed", "1660 sw 48th ave", "Fort Myers", "FL", "33317", "9545130066");
+
+        // Act
         addressBook.add(instance);
         addressBook.remove(0);
+
+        // Assert
+        assertEquals(expectedPerson, instance);
     }
 
     @Test
@@ -30,42 +40,48 @@ class AddressBookTest {
 
     @Test
     void set() {
+        // Act
         addressBook.add(instance);
         addressBook.set(0, instance);
     }
 
     @Test
     void get() {
+        // Act
         addressBook.add(instance);
         addressBook.get(0);
     }
 
     @Test
     void clear() {
+        // Act
         addressBook.clear();
-
         addressBook.add(instance);
         addressBook.clear();
     }
 
     @Test
     void getRowCount() {
+        // Act
         addressBook.getRowCount();
     }
 
     @Test
     void getColumnCount() {
+        // Act
         addressBook.getColumnCount();
     }
 
     @Test
     void getValueAt() {
+        // Act
         addressBook.add(instance);
         addressBook.getValueAt(0, 0);
     }
 
     @Test
     void getColumnName() {
+        // Act
         addressBook.getColumnName(0);
     }
 }
