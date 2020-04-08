da5c1dad5b57828bc7fc8df1a260ab5fbf8664f6
benjmain <bdfulker2@hotmail.com>
2020-04-07 22:33:43 -0400

    Updated AddressBookGUI.java with correct code

--- src/AddressBook/AddressBookGUI.java
+++ src/AddressBook/AddressBookGUI.java
@@ -3,6 +3,8 @@ package AddressBook;//package GUI;
 //import AddressBook.FileSystem;
 
 import javax.swing.*;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
 import javax.swing.event.DocumentEvent;
 import javax.swing.event.DocumentListener;
 import javax.swing.table.TableRowSorter;
@@ -15,12 +17,16 @@ import java.util.regex.Pattern;
 public class AddressBookGUI extends JFrame {
     private static final long serialVersionUID = 1L;
 
+    private static void createAndShowGUI() {
+        AddressBook addressBook = new AddressBook();
+        AddressBookController controller = new AddressBookController(addressBook);
+        GUI.AddressBookGUI gui = new GUI.AddressBookGUI(controller, addressBook);
+        gui.setVisible(true);
+    }
+
     public static void main(String[] args) throws ClassNotFoundException {
         Class.forName("org.sqlite.JDBC");
-        SwingUtilities.invokeLater(() -> {
-            AddressBookGUI gui = new AddressBookGUI();
-            gui.setVisible(true);
-        });
+        SwingUtilities.invokeLater(() -> createAndShowGUI());
     }
 
     private final AddressBook addressBook = new AddressBook();;
@@ -62,22 +68,20 @@ public class AddressBookGUI extends JFrame {
 
         JMenuBar menuBar = new JMenuBar();
         JMenu file = new JMenu("File");
-        file.setMnemonic('F');
         file.setName("file");
-        newItem.addActionListener(e -> {
-            if (saveItem.isEnabled() && JOptionPane.YES_OPTION != JOptionPane.showConfirmDialog(this,
-                    "Are you sure you want to create a new address book? Any unsaved progress will be lost.",
-                    "New Address Book", JOptionPane.YES_NO_OPTION, JOptionPane.QUESTION_MESSAGE)) {
+        file.setMnemonic('F');
+        newItem.addActionListener(e ->
+        {
+            if (saveItem.isEnabled() && JOptionPane.YES_OPTION != JOptionPane.showConfirmDialog(this, "Are you sure you want to create a new address book? Any unsaved progress will be lost.", "New Address Book", JOptionPane.YES_NO_OPTION, JOptionPane.QUESTION_MESSAGE)) {
                 return;
             }
             controller.clear();
             saveItem.setEnabled(false);
         });
         file.add(newItem);
-        openItem.addActionListener(e -> {
-            if (saveItem.isEnabled() && JOptionPane.YES_OPTION != JOptionPane.showConfirmDialog(this,
-                    "Are you sure you want to open a different address book? Any unsaved progress will be lost.",
-                    "Open Address Book", JOptionPane.YES_NO_OPTION, JOptionPane.QUESTION_MESSAGE)) {
+        openItem.addActionListener(e ->
+        {
+            if (saveItem.isEnabled() && JOptionPane.YES_OPTION != JOptionPane.showConfirmDialog(this, "Are you sure you want to create a new address book? Any unsaved progress will be lost.", "New Address Book", JOptionPane.YES_NO_OPTION, JOptionPane.QUESTION_MESSAGE)) {
                 return;
             }
             final JFileChooser jfc = new JFileChooser();
@@ -89,52 +93,58 @@ public class AddressBookGUI extends JFrame {
                 currentFile = jfc.getSelectedFile();
                 saveItem.setEnabled(false);
             } catch (Exception ex) {
-                JOptionPane.showMessageDialog(this, "Error loading file: " + ex.getMessage(), "Open",
-                        JOptionPane.ERROR_MESSAGE);
+                JOptionPane.showMessageDialog(this, "Error loading file: " + ex.getMessage(), "Open", JOptionPane.ERROR_MESSAGE);
             }
         });
         file.add(openItem);
         saveItem.setEnabled(false);
-        saveItem.addActionListener(e -> {
+        saveItem.addActionListener(e ->
+        {
             if (currentFile == null) {
                 saveAsItem.doClick();
                 return;
             }
+            FileSystem fs = new FileSystem();
             try {
                 controller.save(currentFile);
                 saveItem.setEnabled(false);
             } catch (Exception ex) {
-                JOptionPane.showMessageDialog(this, "Error saving the file: " + ex.getMessage(), "Save",
-                        JOptionPane.ERROR_MESSAGE);
+                JOptionPane.showMessageDialog(this, "Error saving the file: " + ex.getMessage(), "Save", JOptionPane.ERROR_MESSAGE);
             }
         });
-        saveItem.addChangeListener(e -> {
-            // Set saveAsItem with the same state as saveItem
-            saveAsItem.setEnabled(saveItem.isEnabled());
+
+        saveItem.addChangeListener(new ChangeListener() {
+            @Override
+            public void stateChanged(ChangeEvent e) {
+                // Set saveAsItem with the same state as saveItem
+                saveAsItem.setEnabled(saveItem.isEnabled());
+            }
         });
-        file.add(saveItem);
         saveAsItem.setEnabled(false);
-        saveAsItem.addActionListener(e -> {
+        file.add(saveItem);
+        saveAsItem.addActionListener(e ->
+        {
             final JFileChooser jfc = new JFileChooser();
             if (JFileChooser.APPROVE_OPTION != jfc.showSaveDialog(this)) {
                 return;
             }
             currentFile = jfc.getSelectedFile();
-            if (currentFile.exists() && JOptionPane.YES_OPTION != JOptionPane.showConfirmDialog(this,
-                    "Are you sure you want to overwrite this file?", "Are you sure?", JOptionPane.YES_NO_OPTION,
-                    JOptionPane.QUESTION_MESSAGE)) {
+            if (currentFile == null) {
+                return;
+            }
+            if (currentFile.exists() && JOptionPane.YES_OPTION != JOptionPane.showConfirmDialog(this, "Are you sure you want to overwrite this file?", "Are you sure?", JOptionPane.YES_NO_OPTION, JOptionPane.QUESTION_MESSAGE)) {
                 return;
             }
             saveItem.doClick();
         });
         file.add(saveAsItem);
         file.add(new JSeparator());
-        printItem.addActionListener(e -> {
+        printItem.addActionListener(e ->
+        {
             try {
                 nameList.print();
             } catch (Exception ex) {
-                JOptionPane.showMessageDialog(this, "Printing failed: " + ex.getMessage(), "Print",
-                        JOptionPane.WARNING_MESSAGE);
+                JOptionPane.showMessageDialog(this, "Printing failed: " + ex.getMessage(), "Print", JOptionPane.WARNING_MESSAGE);
             }
         });
         file.add(printItem);
@@ -164,14 +174,17 @@ public class AddressBookGUI extends JFrame {
             }
 
             public void filter() {
+
                 tableRowSorter.setRowFilter(RowFilter.regexFilter("(?iu)" + Pattern.quote(searchTextField.getText())));
             }
         });
         menuBar.add(searchTextField);
 
+
         JPanel addEditDelPanel = new JPanel();
         addButton.setMnemonic('A');
-        addButton.addActionListener(e -> {
+        addButton.addActionListener(e ->
+        {
             PersonDialog dialog = new PersonDialog(this);
             if (dialog.showDialog() != PersonDialog.Result.OK || dialog.getPerson() == null) {
                 return;
@@ -186,6 +199,7 @@ public class AddressBookGUI extends JFrame {
             if (selectedRow == -1) {
                 return;
             }
+
             // TODO: This doesn't work yet
             int index = nameList.convertRowIndexToModel(selectedRow);
             Person oldPerson = controller.get(index);
@@ -224,11 +238,8 @@ public class AddressBookGUI extends JFrame {
             public void windowClosing(WindowEvent e) {
                 super.windowClosing(e);
                 JFrame frame = (JFrame) e.getSource();
-                // We use saveItem.isEnabled to indicate whether there are unsaved changes or
-                // not
-                if (!saveItem.isEnabled() || JOptionPane.showConfirmDialog(frame,
-                        "Are you sure you want to exit? Your changes will be lost.", "Exit", JOptionPane.YES_NO_OPTION,
-                        JOptionPane.QUESTION_MESSAGE) == JOptionPane.YES_OPTION) {
+                // We use saveItem.isEnabled to indicate whether there are unsaved changes or not
+                if (!saveItem.isEnabled() || JOptionPane.showConfirmDialog(frame, "Are you sure you want to exit? Your changes will be lost.", "Exit", JOptionPane.YES_NO_OPTION, JOptionPane.QUESTION_MESSAGE) == JOptionPane.YES_OPTION) {
                     frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
                 } else {
                     frame.setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
